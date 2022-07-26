a80793f228d303760cbdf31e085ece7f0aba8e57
Koki Kubota <39072144+KokiKubota2@users.noreply.github.com>
2022-07-27 00:16:17 +0900

    bucket名をサーバー側で取得

--- src/upload.js
+++ src/upload.js
@@ -1,24 +1,17 @@
 const _ = require('lodash')
 const axios = require('axios')
 
-module.exports = async (metadata, files, dataUrls, bucketName, endpoint) => {
+module.exports = async (metadata, files, dataUrls, endpoint) => {
   if (!metadata) throw new Error('metadata is empty')
   if (_.isEmpty(files)) throw new Error('files is empty')
   if (_.isEmpty(dataUrls)) throw new Error('dataUrls is empty')
-  if (!bucketName) throw new Error('bucketName is invalid')
   if (!endpoint) throw new Error('endpoint is invalid')
 
-  try {
-    const res = await axios.post(`${endpoint}/api/rev-store/upload`, {
-      metadata,
-      files,
-      dataUrls,
-      bucketName,
-    })
+  const res = await axios.post(`${endpoint}/api/rev-store/upload`, {
+    metadata,
+    files,
+    dataUrls,
+  })
 
-    return res.data.jwt
-  } catch (e) {
-    console.log(e)
-    return null
-  }
+  return res.data.jwt
 }
